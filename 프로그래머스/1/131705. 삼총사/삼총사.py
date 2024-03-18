dc13d01ef15cb08d87ffc1c3d0b44b1b3b00137e
Jeongmin <105720658+Ahn-Jeongmin@users.noreply.github.com>
2024-03-18 19:10:36 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -2,7 +2,7 @@ def solution(number):
     answer = 0
     for i in range(len(number)-2):
         for j in range(i + 1, len(number)-1):
-            for z in range(j+1,len(number)):
-                if number[i]+number[j]+number[z]==0:
-                    answer+=1
+            tmp=number[j+1:].count(-(number[i]+number[j]))
+            answer+=tmp
+                    
     return answer
