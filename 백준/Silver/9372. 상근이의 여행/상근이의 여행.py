26a9a29836a56decf1f92a69368d8b71a31d1621
Jeongmin <105720658+Ahn-Jeongmin@users.noreply.github.com>
2024-08-13 23:13:30 +0900

    [Silver IV] Title: 상근이의 여행, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+count = int(input())
+for _ in range(count):
+    country, num = map(int, input().split())
+    for _ in range(num):
+        x= input()
+    print(country-1)
