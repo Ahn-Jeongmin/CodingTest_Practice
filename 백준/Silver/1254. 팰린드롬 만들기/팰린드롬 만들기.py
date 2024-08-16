5c5e57241e069755713dea495cd0cfa7fade4c4e
Jeongmin <105720658+Ahn-Jeongmin@users.noreply.github.com>
2024-08-16 22:30:22 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,12 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+def shortest_palindrome_length(s):
+    n = len(s)
+    for i in range(n):
+        if is_palindrome(s[i:]):
+            return n + i
+    return 2 * n - 1
+
+s = input().strip()
+print(shortest_palindrome_length(s))
